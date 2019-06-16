02349673f3970f04873c45643af7434bd964cab7
Aashna <bajwa.aashna@gmail.com>
2019-06-15 22:35:36 -0400

    Started, keep going champ.

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,4 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Bins {
 
+    private Map<Integer, Integer> binMap = new HashMap<Integer, Integer>();
+
+    public Bins(Integer minimum, Integer maximum){
+        for (int i = minimum; i < maximum + 1; i++){
+            this.binMap.put(i,0);
+        }
+    }
 }
